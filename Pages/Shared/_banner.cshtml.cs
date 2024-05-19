919ca84b659bec1b27ddff0a4ae1c65dd46744ed
vanhuy.le <vanhuy.le@wiztechww.com>
2024-05-19 23:50:46 +0700

    update-ui-home

---
+++ Pages/Shared/_banner.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace lhs.Pages.Shared
+{
+    public class bannerModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
